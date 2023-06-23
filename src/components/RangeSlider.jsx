358f405b2fe2f4ffe5e2fd58157e6669c81d9996
Gerhard <gerhard.putter@yahoo.com>
2023-06-23 15:13:22 +0200

    Add functions for range slider.

--- src/components/RangeSlider.jsx
+++ src/components/RangeSlider.jsx
@@ -6,15 +6,47 @@ function RangeSlider({ min, max, value, step }) {
     const [inputValue, setInputValue] = useState(value);
     const sliderRef = useRef(null)
 
+    function handleSliderInput() {
+        const range = max - min;
+        const distance = sliderRef.current.value - min;
+        const percentage = (distance / range) * 100;
+        setSliderRange(percentage)
+        setInputValue(sliderRef.current.value)
+    }
+
+    function handleNumberInput(e) {
+        const newValue = parseInt(e.target.value);
+        if (newValue < min) {
+            setInputValue(min)
+            setSliderRange(0)
+        } else if (newValue > max) {
+            setInputValue(max)
+            setSliderRange(100)
+        } else {
+            setInputValue(newValue);
+            const range = max - min;
+            const distance = newValue - min;
+            const percentage = (distance / range) * 100;
+            setSliderRange(percentage)
+        }
+    }
+
+    useEffect(() => {
+        handleSliderInput()
+    }, [sliderRef])
+
+
     return (
         <div className="range-slider">
             <div className="slider-values">
-                <input type="number" className="number-input" />
+                <small>{min}</small>
+                <input type="number" className="number-input" min={min} max={max} step={step} onInput={handleNumberInput} />
+                <small>{max}</small>
             </div>
             <div className="slider-container">
-                <input type="range" className="slider" />
-                <div className="slider-thumb"></div>
-                <div className="progress"></div>
+                <input type="range" className="slider" min={min} max={max} ref={sliderRef} step={step} onInput={handleSliderInput} />
+                <div className="slider-thumb" style={{ left: `calc(${sliderRange}% - 0.5em)` }}></div>
+                <div className="progress" style={{ width: `${sliderRange}%` }}></div>
             </div>
         </div>
     );
